4cbd5edb1f6148bf960c4cf4d461f0410474aec7
Admin <conner.p.mccabe@gmail.com>
2019-11-15 14:35:58 -0600

    simplified simple button

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import './App.css';
 export default function Button() {
   const [buttonText, setButtonText] = useState('Click me, plz');
 
+  function handleClick() {
+    return setButtonText("Thanks, been clicked!");
+  }
   return (
-    <button onClick={ () => setButtonText("Thanks, been clicked")}>
+    <button onClick={handleClick}>
       {buttonText}
     </button>
   )
